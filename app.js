75c431c50f7d468cba80c8870d14c029bc6c0e1a
Ahmad Yusuf <35657456+Ahmad-opera@users.noreply.github.com>
2021-02-02 00:57:56 +0100

    df

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.get("/get-cookies", (req, res) => {
 
 app.use("/api/user", authRoute);
 app.use("/user", userRoute);
-
-app.listen(3000 || process.env.PORT, () => {
-  console.log("Listening on port 3000...");
+const port = 3000 || process.env.PORT;
+app.listen(port, () => {
+  console.log("Listening on port " + port + "...");
 });
