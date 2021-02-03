6dfb7acb44fd71a0c46534de35a52d1a6fa19c48
Ahmad Yusuf <35657456+Ahmad-opera@users.noreply.github.com>
2021-02-03 16:18:30 +0100

    Commit

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,12 @@
-const router = require("express").Router();
+const express = require("express");
+const router = express.Router();
 const verify = require("./verifyToken");
 const userController = require("../controller/user");
 const axios = require("axios");
 
-router.get("/dashboard", verify, userController.invest_get);
-router.get("/makeinvest", userController.invest_post);
-router.get("/gettransaction", userController.gen_transaction);
+ router.get("/dashboard", verify, userController.invest_get);
+ // router.get("/makeinvest", verify, userController.invest_post);
+  router.get("/gettransaction", userController.gen_transaction);
 router.get("/notify", userController.notify_invoice_success);
 
 module.exports = router;
